ea7aa6a926834fb5b5e48abe0256dc8eb43ec06a
HariBalaji4674 <Hari.Balaji4674@gmail.com>
2023-11-15 08:55:47 +0530

    Adding New Files

---
+++ 01-Day.sh
@@ -0,0 +1,53 @@
+What is DevOps?
+    DevOps is the combination of cultural Philosophies,Practices and Tools 
+    That increases the ability to deliver the application and its servces at hight velocity with evolve and improving products at faster pace then the normal software development process and infrastructure management process.
+    This Speed Enables Organisations to better serve their customers and Compete with peers in the market
+What are the benefits of DevOps? What can it help us to achieve?
+    Collaboration 
+    Speed
+    Security
+    Continious Delivery
+    Continious Deployment
+    Continious Integration
+    Scalability
+    Reliability
+    Upgradations
+What is Software Distributions?
+    Software Distributio is the process of delivering the softewware and its components to its end user
+    Ditro means the collection of software tools, build ,assemble and configured so that it can be used efficiently as is it is often a closest thing of turnkey of free open source software
+
+What is Software?
+    A set of intructions ,data and programs that are implemneted in the hardware part of computer used to operate computers and execute specific tasks which user has need .
+    it Solves complex problems and saves lots of time to the humans by solving manual taks easily
+
+What are the ways to distribute the software and advantages vs disadvantages?
+    Version control(Source Code Management) --> Required More tools to set up like Git,GitHub,Jenkins,
+    Package Management
+    Archieve
+    Image Building
+
+What is Caching? Types of Caching? What are its uses ?
+    Caching is storing the minimum data required to enter the appications which aused has accessed very frequently 
+    which are highly expensive to eneter everytime and io intensive the data could not change very often (Very Frequently)
+    There are several layers in Caching CPU Caching,Browser Caching,Ddistributed systems Caching, etc.. 
+    Common Ones are Memory Caching and Distributed Caching
+
+what is Open Source?
+    The term open source refers to something people can modify and share because its design is publicly accessible.
+
+Describe the architecture of service/app/project/... you designed and/or implemented?
+    Explain The Roboshop Implementation.
+
+What does "Availability" mean? What means are there to track Availability of a service?
+    Service availability is simply the measure of the service being available and accessible to the customers during the time you promised to keep the service available. It's usually calculated as a percentage.
+
+Why isn't 100% availability a target? Why do most companies or teams set it to be 99%.X?
+
+Describe the workflow of setting up some type of web server (Apache, IIS, Tomcat, ...)?
+
+What is Choas Engineering?
+    The Engineering team who are doing experiments on the production Systems and services to build confidence on the Productions systems that ability of the software capability. 
+    build confidence in the system's capability to withstand turbulent and unexpected conditions"
+    
+What is "infrastructure as code"? What implementation of IAC are you familiar with?
+
